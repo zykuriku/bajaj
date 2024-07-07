4d7a0c890886e64fd476221b95a607675abdc9f9
zykuriku <kavyapandey2002@gmail.com>
2024-07-08 00:18:18 +0530

    commit 19

--- src/main/java/com/kavya/api/controller/ApiControllers.java
+++ src/main/java/com/kavya/api/controller/ApiControllers.java
@@ -6,7 +6,9 @@ import com.kavya.api.repo.SeriesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiControllers {
@@ -18,7 +20,7 @@ public class ApiControllers {
     @GetMapping(value = "/")
     public String getPage()
     {
-        return "Welcome";
+        return "Welcome to Wpick API wherein you can have fun with loads of international and Indian Web Series!";
     }
 
     @GetMapping(value = "/series")
@@ -29,13 +31,22 @@ public class ApiControllers {
         return all;
     }
 
+    @GetMapping(value = "/series/name={name}")
+
+    public WebSeries getSeriesByName(@PathVariable String name)
+    {
+
+        return seriesRepo.findByName(name);
+    }
+
+
     @PostMapping(value = "/save")
     public String saveSeries(@RequestBody WebSeries series){
         seriesRepo.save(series);
         return "saved..";
     }
 
-    @PutMapping(value = "/delete/{id}")
+    @PutMapping(value = "/update/id={id}")
     public String updateSeries(@PathVariable long id, @RequestBody WebSeries series){
         WebSeries updatedSeries= seriesRepo.findById(id).get();
         updatedSeries.setDescription(series.getDescription());
@@ -44,13 +55,19 @@ public class ApiControllers {
         updatedSeries.setYear(series.getYear());
         updatedSeries.setRating(series.getRating());
         updatedSeries.setSeasons(series.getSeasons());
+        updatedSeries.setCast(series.getCast());
+        updatedSeries.setAllPlatforms(series.getAllPlatforms());
+        updatedSeries.setEpisodesPerSeason(series.getEpisodesPerSeason());
+        updatedSeries.setLanguages(series.getLanguages());
+        updatedSeries.setPlatformsFree(series.getPlatformsFree());
         seriesRepo.save(series);
         return "updated..";
     }
+    @DeleteMapping(value = "/delete/id={id}")
 
     public String deleteSeries(@PathVariable long id){
         WebSeries deletedSeries = seriesRepo.findById(id).get();
-        seriesRepo.deleteById(id);
+        seriesRepo.delete(deletedSeries);
         return "Delete series with id: "+id;
     }
 
--- src/main/java/com/kavya/api/repo/SeriesRepo.java
+++ src/main/java/com/kavya/api/repo/SeriesRepo.java
@@ -3,5 +3,10 @@ package com.kavya.api.repo;
 import com.kavya.api.models.WebSeries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SeriesRepo extends JpaRepository<WebSeries, Long> {
+
+    WebSeries findByName(String name);
 }
